aa5e9ffe081f89f37473a83924814a5b7fdad4ae
zishaan1911 <iamzishaan@gmail.com>
2026-05-01 16:58:17 +0000

    Solve Regular Expression Matching (cpp)

--- regular-expression-matching/solutions/solution.cpp
+++ regular-expression-matching/solutions/solution.cpp
@@ -1,39 +1,33 @@
 class Solution {
+    signed char memo[21][21]; 
+
 public:
     bool isMatch(string s, string p) {
-        int m = s.length();
-        int n = p.length();
-        
-        // dp[i][j] means s[0...i-1] matches p[0...j-1]
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        
-        dp[0][0] = true;
+        for (int i = 0; i <= s.length(); i++)
+            for (int j = 0; j <= p.length(); j++)
+                memo[i][j] = -1;
+                
+        return dp(0, 0, s, p);
+    }
 
-        // Initialize first row for patterns like a*, a*b*, etc.
-        for (int j = 2; j <= n; j++) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 2];
+    bool dp(int i, int j, const string& s, const string& p) {
+        if (memo[i][j] != -1) return memo[i][j];
+        
+        bool result;
+        if (j == p.length()) {
+            result = (i == s.length());
+        } else {
+            bool first_match = (i < s.length() && 
+                               (p[j] == s[i] || p[j] == '.'));
+
+            if (j + 1 < p.length() && p[j + 1] == '*') {
+                result = dp(i, j + 2, s, p) || 
+                         (first_match && dp(i + 1, j, s, p));
+            } else {
+                result = first_match && dp(i + 1, j + 1, s, p);
             }
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
-                    // Current characters match
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else if (p[j - 1] == '*') {
-                    // Zero occurrences of the preceding element
-                    dp[i][j] = dp[i][j - 2];
-                    
-                    // One or more occurrences
-                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
-                }
-            }
-        }
-
-        return dp[m][n];
+        
+        return memo[i][j] = result;
     }
 };
