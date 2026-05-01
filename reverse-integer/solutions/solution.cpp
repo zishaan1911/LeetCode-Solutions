5bfb9fed04d63f1c1a6fa2f1b3a49336a8001d79
zishaan1911 <iamzishaan@gmail.com>
2026-05-01 17:25:22 +0000

    Solve Reverse Integer (cpp)

--- reverse-integer/solutions/solution.cpp
+++ reverse-integer/solutions/solution.cpp
@@ -4,11 +4,7 @@ public:
         if (x == 0) return x;
         int rev = 0;
         while (x != 0) {
-            if (rev > INT_MAX / 10 || rev == INT_MAX / 10 && (x % 10) > 7) {
-                return 0;
-            }
-
-            if (rev < INT_MIN / 10 || rev == INT_MIN / 10 && (x % 10) < -8) {
+            if ((rev > INT_MAX / 10 || rev == INT_MAX / 10 && (x % 10) > 7) || (rev < INT_MIN / 10 || rev == INT_MIN / 10 && (x % 10) < -8)) {
                 return 0;
             }
 
