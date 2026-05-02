e6d823acb81b9e193e7dfdf0012443b48aecdac3
zishaan1911 <iamzishaan@gmail.com>
2026-05-02 12:44:32 +0000

    Solve Merge K Sorted Lists (cpp)

---
+++ merge-k-sorted-lists/README.md
@@ -0,0 +1,3 @@
+# Merge K Sorted Lists
+
+https://leetcode.com/problems/merge-k-sorted-lists/
---
+++ merge-k-sorted-lists/solutions/solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        
+        int k = lists.size();
+        while (k > 1) {
+            for (int i = 0; i < k / 2; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[k - 1 - i]);
+            }
+            k = (k + 1) / 2;
+        }
+        return lists[0];
+    }
+
+private:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
