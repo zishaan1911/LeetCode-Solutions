370ae236eddd949400793ceeb818e639c9970a80
zishaan1911 <iamzishaan@gmail.com>
2026-05-02 15:57:38 +0000

    Solve Find The Index Of The First Occurrence In A String (cpp)

--- find-the-index-of-the-first-occurrence-in-a-string/solutions/solution.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string/solutions/solution.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         for (int i = 0; i < haystack.size(); i++) {
-            if (haystack[i] != needle[0]) continue;
             string o = haystack.substr(i, needle.size());
             if (o == needle) {
                 return i;
