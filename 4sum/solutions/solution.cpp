164e7455942fe2c492f91f0105581beaf19ae2cf
zishaan1911 <iamzishaan@gmail.com>
2026-05-02 12:06:33 +0000

    Solve 4Sum (cpp)

--- 4sum/solutions/solution.cpp
+++ 4sum/solutions/solution.cpp
@@ -5,29 +5,31 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for (int j = i + 1; j < n; j++) {
+            if ((long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
+            
+            if ((long long)nums[i] + nums[n-3] + nums[n-2] + nums[n-1] < target) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if ((long long)nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
+                if ((long long)nums[i] + nums[j] + nums[n-2] + nums[n-1] < target) continue;
 
-                int front = j + 1;
-                int back = n - 1;
-
-                while (front < back) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[front] + nums[back];
-
-                    if (sum < target) {
-                        front++;
-                    } else if (sum > target) {
-                        back--;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
                     } else {
-                        result.push_back({nums[i], nums[j], nums[front], nums[back]});
-                        while (front < back && nums[front] == nums[front + 1]) front++;
-                        while (front < back && nums[back] == nums[back - 1]) back--;
-
-                        front++;
-                        back--;
+                        right--;
                     }
                 }
             }
