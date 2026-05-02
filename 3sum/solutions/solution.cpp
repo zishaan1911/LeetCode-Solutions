d68cfeaf72a3028053fdb62c6fab5297e3739786
zishaan1911 <iamzishaan@gmail.com>
2026-05-02 05:40:45 +0000

    Solve 3Sum (cpp)

---
+++ 3sum/solutions/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+
+            if (nums[i] + nums[n - 2] + nums[n - 1] < 0) continue;
+
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[++left]);
+                    while (left < right && nums[right] == nums[--right]);
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+};
