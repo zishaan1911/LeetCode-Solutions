8106587f591ca4c72b483ee259b3b3db9ba8d2c7
zishaan1911 <iamzishaan@gmail.com>
2026-05-09 14:38:12 +0000

    Solve Score Validator (cpp)

--- score-validator/solutions/solution.cpp
+++ score-validator/solutions/solution.cpp
@@ -1,25 +1,27 @@
+#include <vector>
+#include <string>
+
 class Solution {
 public:
-    vector<int> scoreValidator(vector<string>& events) {
-       int score = 0;
-    int counter = 0;
+    std::vector<int> scoreValidator(std::vector<std::string>& events) {
+        int score = 0;
+        int counter = 0;
 
-    for (const std::string& event : events) {
-        // Stop processing if we reach 10 wickets
-        if (counter == 10) {
-            break;
+        for (const std::string& event : events) {
+            if (counter == 10) {
+                break;
+            }
+
+            if (event == "W") {
+                counter++;
+            } else if (event == "WD" || event == "NB") {
+                score += 1;
+            } else {
+                // The event is a number ("0", "1", "2", "3", "4", "6")
+                score += std::stoi(event);
+            }
         }
 
-        if (event == "W") {
-            counter++;
-        } else if (event == "WD" || event == "NB") {
-            score += 1;
-        } else {
-            // This handles "0", "1", "2", "3", "4", "6"
-            score += std::stoi(event);
-        }
-    }
-
-    return {score, counter}; 
+        return {score, counter};
     }
 };
