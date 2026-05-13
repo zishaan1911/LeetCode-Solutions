69abc9a0088fe267e46fa20ddbe76d8ef8fa4008
zishaan1911 <iamzishaan@gmail.com>
2026-05-13 14:44:02 +0000

    Solve Spiral Matrix Ii (cpp)

--- spiral-matrix-ii/solutions/solution.cpp
+++ spiral-matrix-ii/solutions/solution.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> matrix(n, vector<int>(n));
+        vector<vector<int>> res(n, vector<int>(n));
         
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
+        int r1 = 0, r2 = n - 1;
+        int c1 = 0, c2 = n - 1;
         int val = 1;
-
-        while (val <= n * n) {
-            for (int i = left; i <= right; ++i) {
-                matrix[top][i] = val++;
-            }
-            top++;
-
-            for (int i = top; i <= bottom; ++i) {
-                matrix[i][right] = val++;
-            }
-            right--;
-            for (int i = right; i >= left; --i) {
-                matrix[bottom][i] = val++;
-            }
-            bottom--;
-            for (int i = bottom; i >= top; --i) {
-                matrix[i][left] = val++;
-            }
-            left++;
-        }
         
-        return matrix;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; ++c) res[r1][c] = val++;
+            
+            for (int r = r1 + 1; r <= r2; ++r) res[r][c2] = val++;
+            
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; --c) res[r2][c] = val++;
+                for (int r = r2; r > r1; --r) res[r][c1] = val++;
+            }
+            
+            r1++; r2--;
+            c1++; c2--;
+        }
+        return res;
     }
 };
