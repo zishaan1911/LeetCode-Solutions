f6d031883c53aa7f4527a06ece3e575faadaacdd
zishaan1911 <iamzishaan@gmail.com>
2026-05-14 20:03:34 +0000

    Solve Climbing Stairs (cpp)

--- climbing-stairs/solutions/solution.cpp
+++ climbing-stairs/solutions/solution.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-        int result = (pow(((1 + sqrt(5)) / 2), n + 1) - pow(((1 - sqrt(5)) / 2), n + 1)) / sqrt(5);
-        
-        return round(result);
+        return round((pow(((1 + sqrt(5)) / 2), n + 1) - pow(((1 - sqrt(5)) / 2), n + 1)) / sqrt(5));
     }
 };
