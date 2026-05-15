fc55285ec43fb6d40f74685cf8f91c5ac1caf654
zishaan1911 <iamzishaan@gmail.com>
2026-05-15 15:10:30 +0000

    Solve Sort Colors (cpp)

--- sort-colors/solutions/solution.cpp
+++ sort-colors/solutions/solution.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int end = nums.size();
-        for (int i = 0; i < end; i++) {
-            if (!nums[i]) {
-                nums.erase(nums.begin() + i);
-                nums.insert(nums.begin(), 0);
-            }
-            else if (nums[i] == 2) {
-                nums.erase(nums.begin() + i);
-                nums.push_back(2);
-                i--;
-                end--;
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
