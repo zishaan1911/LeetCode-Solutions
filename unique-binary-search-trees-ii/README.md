2e81ca9342f56af49f38d889dfa15a50d4b3e5d5
zishaan1911 <iamzishaan@gmail.com>
2026-05-23 17:44:56 +0000

    Solve Unique Binary Search Trees Ii (cpp)

---
+++ unique-binary-search-trees-ii/README.md
@@ -0,0 +1,3 @@
+# Unique Binary Search Trees Ii
+
+https://leetcode.com/problems/unique-binary-search-trees-ii/
---
+++ unique-binary-search-trees-ii/solutions/solution.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return buildTrees(1, n);
+    }
+
+private:
+    vector<TreeNode*> buildTrees(int start, int end) {
+        vector<TreeNode*> ans;
+        
+        if (start > end) {
+            ans.push_back(nullptr);
+            return ans;
+        }
+        
+        for (int i = start; i <= end; ++i) {
+            vector<TreeNode*> leftSubtrees = buildTrees(start, i - 1);
+            vector<TreeNode*> rightSubtrees = buildTrees(i + 1, end);
+            
+            for (TreeNode* left : leftSubtrees) {
+                for (TreeNode* right : rightSubtrees) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    ans.push_back(root);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
