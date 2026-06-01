daf0a9493dcc3c2dfb6666b0c1d0183a07cb4a6c
zishaan1911 <iamzishaan@gmail.com>
2026-06-01 20:10:15 +0000

    Solve Balanced Binary Tree (cpp)

---
+++ balanced-binary-tree/README.md
@@ -0,0 +1,3 @@
+# Balanced Binary Tree
+
+https://leetcode.com/problems/balanced-binary-tree/
---
+++ balanced-binary-tree/solutions/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+
+private:
+    int checkHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node->left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = checkHeight(node->right);
+        if (rightHeight == -1) return -1;
+
+        if (abs(leftHeight - rightHeight) > 1) {
+            return -1; 
+        }
+
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
