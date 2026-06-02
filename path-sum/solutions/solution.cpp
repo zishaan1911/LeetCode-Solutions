aaf52c35e87ab8b2fbe64bb993b3304eaf3a225d
zishaan1911 <iamzishaan@gmail.com>
2026-06-02 15:49:50 +0000

    Solve Path Sum (cpp)

---
+++ path-sum/solutions/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) {
+            return false;
+        }
+        
+        if (root->left == nullptr && root->right == nullptr) {
+            return root->val == targetSum;
+        }
+        
+        int remainingSum = targetSum - root->val;
+        return hasPathSum(root->left, remainingSum) || hasPathSum(root->right, remainingSum);
+    }
+};
