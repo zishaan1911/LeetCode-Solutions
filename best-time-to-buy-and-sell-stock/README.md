1f877648e23c29f3265be34af5117c77971cfb7b
zishaan1911 <iamzishaan@gmail.com>
2026-06-10 16:08:21 +0000

    Solve Best Time To Buy And Sell Stock (cpp)

---
+++ best-time-to-buy-and-sell-stock/README.md
@@ -0,0 +1,3 @@
+# Best Time To Buy And Sell Stock
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
---
+++ best-time-to-buy-and-sell-stock/solutions/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            else {
+                int currentProfit = prices[i] - minPrice;
+                maxProfit = max(maxProfit, currentProfit);
+            }
+        }
+
+        return maxProfit;
+    }
+};
