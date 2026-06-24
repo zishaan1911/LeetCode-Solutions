4ce432a83b5ea2b1be6d5fed3e5516d3124d363f
zishaan1911 <iamzishaan@gmail.com>
2026-06-24 04:33:58 +0000

    Solve Linked List Cycle Ii (cpp)

--- linked-list-cycle-ii/solutions/solution.cpp
+++ linked-list-cycle-ii/solutions/solution.cpp
@@ -1,15 +1,34 @@
+#include <cstdint>
+#include <vector>
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode *slow = head, *fast = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast) {
-                while (head != slow) head = head->next, slow = slow->next;
-                return head;
+        if (!head || !head->next) return nullptr;
+
+        const size_t BUCKET_COUNT = 2048;
+        std::vector<uintptr_t> buckets[BUCKET_COUNT];
+
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            uintptr_t addr = reinterpret_cast<uintptr_t>(curr);
+            
+            uintptr_t compressed_addr = addr >> 4;
+
+            size_t bucket_idx = compressed_addr & (BUCKET_COUNT - 1);
+
+            for (uintptr_t stored_addr : buckets[bucket_idx]) {
+                if (stored_addr == addr) {
+                    return curr;
+                }
             }
+
+            buckets[bucket_idx].push_back(addr);
+
+            curr = curr->next;
         }
+
         return nullptr;
     }
 };
