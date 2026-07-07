07852ca7d64de3732935f5d8a9a4cf2b026b8188
zishaan1911 <iamzishaan@gmail.com>
2026-07-07 04:35:15 +0000

    Solve Fraction To Recurring Decimal (cpp)

--- fraction-to-recurring-decimal/solutions/solution.cpp
+++ fraction-to-recurring-decimal/solutions/solution.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-        
         if (numerator == 0) return "0";
+        
         string res;
-        if ((numerator < 0) ^ (denominator < 0)) res.push_back('-');
+        if ((numerator < 0) ^ (denominator < 0)) res += '-';
 
-        long long n = llabs((long long)numerator);
-        long long d = llabs((long long)denominator);
+        long long n = abs((long long)numerator);
+        long long d = abs((long long)denominator);
 
         res += to_string(n / d);
         long long rem = n % d;
         if (rem == 0) return res;
 
-        res.push_back('.');
+        res += '.';
+        
         unordered_map<long long, int> seen;
-
+        string fractional_part;
+        
         while (rem != 0) {
-            if (seen.find(rem) != seen.end()) {
-                int pos = seen[rem];
-                res.insert(pos, "(");
-                res.push_back(')');
+            if (seen.count(rem)) {
+                fractional_part.insert(seen[rem], "(");
+                fractional_part += ')';
                 break;
             }
-            seen[rem] = res.size();
+            
+            seen[rem] = fractional_part.size();
             rem *= 10;
-            int digit = rem / d;
-            res.push_back(char('0' + digit));
-            rem = rem % d;
+            fractional_part += to_string(rem / d);
+            rem %= d;
         }
-        return res;
+        
+        return res + fractional_part;
     }
 };
