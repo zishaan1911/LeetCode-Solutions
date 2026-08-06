cce47fcf42a67c96de13fd4fe47c5731355f507d
zishaan1911 <iamzishaan@gmail.com>
2026-08-06 12:28:20 +0000

    Solve Kth Largest Element In An Array (cpp)

--- kth-largest-element-in-an-array/solutions/solution.cpp
+++ kth-largest-element-in-an-array/solutions/solution.cpp
@@ -1,32 +1,7 @@
 class Solution {
 public:
-    int findKthLargest(std::vector<int>& nums, int k) {
-        int left = 0, right = nums.size() - 1;
-        while (true) {
-            int pivot_index = rand() % (right - left + 1) + left;
-            int new_pivot_index = partition(nums, left, right, pivot_index);
-            if (new_pivot_index == nums.size() - k) {
-                return nums[new_pivot_index];
-            } else if (new_pivot_index > nums.size() - k) {
-                right = new_pivot_index - 1;
-            } else {
-                left = new_pivot_index + 1;
-            }
-        }
-    }
-
-private:
-    int partition(vector<int>& nums, int left, int right, int pivot_index) {
-        int pivot = nums[pivot_index];
-        swap(nums[pivot_index], nums[right]);
-        int stored_index = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivot) {
-                swap(nums[i], nums[stored_index]);
-                stored_index++;
-            }
-        }
-        swap(nums[right], nums[stored_index]);
-        return stored_index;
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k-1];
     }
 };
