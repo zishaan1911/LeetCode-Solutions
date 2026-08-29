d37760620bccc7c6fa8855ce31d461c648dc1843
zishaan1911 <iamzishaan@gmail.com>
2026-08-29 12:00:36 +0000

    Solve Range Sum Query Immutable (cpp)

--- range-sum-query-immutable/solutions/solution.cpp
+++ range-sum-query-immutable/solutions/solution.cpp
@@ -1,16 +1,21 @@
 class NumArray {
 public:
     vector<int> nums;
+    int totalSum = 0;
     NumArray(vector<int>& num) {
         for (int i = 0; i < num.size(); i++) {
             nums.push_back(num[i]);
+            totalSum += num[i];
         }
     }
     
     int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum += nums[i];
+        int sum = totalSum;
+        for (int i = 0; i < left; i++) {
+            sum -= nums[i];
+        }
+        for (int i = right + 1; i < nums.size(); i++) {
+            sum -= nums[i];
         }
         return sum;
     }
